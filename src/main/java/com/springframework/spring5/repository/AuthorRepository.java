13532b968a4ccfa3d548e3c135449c0562f9fdf3
Ravi Ranjan <ravi2519@gmail.com>
2020-01-18 07:20:32 +0530

    JPA Repositories for Crud Operations added

---
+++ src/main/java/com/springframework/spring5/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.springframework.spring5.repository;
+
+import com.springframework.spring5.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
